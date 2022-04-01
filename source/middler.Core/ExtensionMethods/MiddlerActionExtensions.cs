9dd79cfa522ca70891965c839dc037eafc2b7c36
windischb <bernhard.windisch@doob.at>
2022-04-01 22:47:10 +0200

    ...

--- source/middler.Core/ExtensionMethods/MiddlerActionExtensions.cs
+++ source/middler.Core/ExtensionMethods/MiddlerActionExtensions.cs
@@ -5,40 +5,40 @@ using doob.Reflectensions.ExtensionMethods;
 namespace doob.middler.ExtensionMethods {
     public static class MiddlerActionExtensions {
 
-        //public static MiddlerAction ToBasicMiddlerAction<T>(this MiddlerAction<T> middlerAction) where T : class, new()
-        //{
-        //    var act = new MiddlerAction
-        //    {
-        //        Terminating = middlerAction.Terminating,
-        //        WriteStreamDirect = middlerAction.WriteStreamDirect,
-        //        ActionType = middlerAction.ActionType,
-        //        Parameters = Converter.Json.ToJObject(middlerAction.Parameters)
-        //            //middlerAction.Parameters?.GetType().GetProperties()
-        //            //    .ToDictionary(p => p.Name, p => p.GetValue(middlerAction.Parameters)) ??
-        //            //new Dictionary<string, object>()
-        //    };
-
-        //    return act;
-        //}
-
-        public static MiddlerAction ToBasicMiddlerAction(this IMiddlerAction middlerAction)
+        public static MiddlerAction ToBasicMiddlerAction<T>(this MiddlerAction<T> middlerAction) where T : class, new()
         {
             var act = new MiddlerAction
             {
                 Terminating = middlerAction.Terminating,
                 WriteStreamDirect = middlerAction.WriteStreamDirect,
-                ActionType = middlerAction.ActionType
+                ActionType = middlerAction.ActionType,
+                Parameters = Converter.Json.ToJObject(middlerAction.Parameters)
+                //middlerAction.Parameters?.GetType().GetProperties()
+                //    .ToDictionary(p => p.Name, p => p.GetValue(middlerAction.Parameters)) ??
+                //new Dictionary<string, object>()
             };
-
-            var parametersProperty = middlerAction.GetType().GetProperty("Parameters");
-            if (parametersProperty != null)
-            {
-                var parameters = parametersProperty.GetValue(middlerAction)!;
-                act.Parameters = Converter.Json.ToJObject(parameters);
-            }
 
             return act;
         }
+
+        //public static MiddlerAction ToBasicMiddlerAction(this IMiddlerAction middlerAction)
+        //{
+        //    var act = new MiddlerAction
+        //    {
+        //        Terminating = middlerAction.Terminating,
+        //        WriteStreamDirect = middlerAction.WriteStreamDirect,
+        //        ActionType = middlerAction.ActionType
+        //    };
+
+        //    var parametersProperty = middlerAction.GetType().GetProperty("Parameters");
+        //    if (parametersProperty != null)
+        //    {
+        //        var parameters = parametersProperty.GetValue(middlerAction)!;
+        //        act.Parameters = Converter.Json.ToJObject(parameters);
+        //    }
+
+        //    return act;
+        //}
 
     }
 }
